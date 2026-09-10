140892e5a308ca6888e48134b30f1d357c78f8dd
DerEineTypDa <manuel.guggenmos9@gmail.com>
2026-09-10 15:36:29 +0200

    Client-side initialization

---
+++ src/main/java/de/manuai/client/ManuAIClient.java
@@ -0,0 +1,10 @@
+package de.manuai.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ManuAIClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// Client-side initialization will go here
+	}
+}
